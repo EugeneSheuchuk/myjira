650b1d6affc640ce0f2254cbdc322f3d868199de
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-17 11:49:03 +0300

    style: add normalize.css

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sidebar.scss';
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div className='Sidebar'>
+
+            </div>
+        );
+    }
+};
+
+export default Sidebar;
