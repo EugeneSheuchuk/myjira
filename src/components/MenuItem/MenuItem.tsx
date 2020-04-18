e7c97bdda915830d4fd08bf15884a161dbd37402
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-18 10:44:10 +0300

    feat: add items to 'Menu' component

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,40 +1,42 @@
-import React, { KeyboardEvent, MouseEvent } from 'react';
+import React, { KeyboardEvent } from 'react';
+import { NavLink } from 'react-router-dom';
 import './MenuItem.scss';
 
 type TProps = {
-  imgURL: string;
-  name: string;
-  action?: Function;
-  tabIndex: number;
+    imgURL: string;
+    name: string;
+    action?: Function;
+    tabIndex: number;
 };
 
 const MenuItem: React.FC<TProps> = ({
-  imgURL,
-  name,
-  action,
-  tabIndex,
-}) => {
+                                        imgURL,
+                                        name,
+                                        action,
+                                        tabIndex,
+                                    }) => {
 
-  const defaultFunc = () => {};
-  const componentAction = action || defaultFunc;
+    const defaultFunc = () => {
+    };
+    const componentAction = action || defaultFunc;
 
-  // eslint-disable-next-line
-  const pressEnter = (e: KeyboardEvent): void => {
-    if (e.key === 'Enter') componentAction(e);
-  };
+    // eslint-disable-next-line
+    const pressEnter = (e: KeyboardEvent): void => {
+        if (e.key === 'Enter') componentAction(e);
+    };
 
-  return (
-    <div
-      className="MenuItem"
-      role="button"
-      onClick={(e: MouseEvent<HTMLDivElement>) => componentAction(e)}
-      onKeyPress={(e: KeyboardEvent) => pressEnter(e)}
-      tabIndex={tabIndex}
-    >
-      <img src={imgURL} className="MenuItem_icon" alt={`${name}`} />
-      <div className="MenuItem_name">{name}</div>
-    </div>
-  );
+    return (
+        <NavLink
+            to='/'
+            className="MenuItem"
+            onKeyPress={(e: KeyboardEvent) => pressEnter(e)}
+            tabIndex={tabIndex}
+            activeClassName='MenuItem_active'
+        >
+            <img src={imgURL} className="MenuItem_icon" alt={`${name}`}/>
+            <div className="MenuItem_name">{name}</div>
+        </NavLink>
+    );
 };
 
 export default MenuItem;
--- src/containers/Menu/Menu.tsx
+++ src/containers/Menu/Menu.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './Menu.scss'
 import MenuItem from '../../components/MenuItem/MenuItem';
 import Settings from '../../assets/images/settings.png';
+import Roadmap from '../../assets/images/roadmap.png';
+import Board from '../../assets/images/board.png';
+import AddItem from '../../assets/images/additem.png';
+import Page from '../../assets/images/page.webp';
 
 type TState = {
     width: number;
@@ -19,7 +23,11 @@ class Menu extends React.Component<{}, TState> {
     render() {
         return (
             <div className='Menu' style={{width: `${this.state.width}px`}}>
-                <MenuItem imgURL={Settings} name='Settings' tabIndex={0}/>
+                <MenuItem imgURL={Roadmap} name='Roadmap' tabIndex={0}/>
+                <MenuItem imgURL={Board} name='Board' tabIndex={0}/>
+                <MenuItem imgURL={Page} name='Pages' tabIndex={0}/>
+                <MenuItem imgURL={AddItem} name='Add item' tabIndex={0}/>
+                <MenuItem imgURL={Settings} name='Property settings' tabIndex={0}/>
             </div>
         );
     }
