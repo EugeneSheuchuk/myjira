ac482c942483b9ccc8edd7ebece0ff8a6f28b807
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-20 10:17:28 +0300

    feat: add board reducer

---
+++ src/store/boardReducer.ts
@@ -0,0 +1,23 @@
+type Task = {
+    taskName: string;
+};
+type BoardTask = {
+    boardName: string;
+    tasks: Array<Task>;
+};
+type Boards = {
+    boards: Array<BoardTask>;
+};
+
+const initialState: Boards = {
+    boards: [],
+};
+
+const boardReducer = (state = initialState, action: any):Boards  => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default boardReducer;
