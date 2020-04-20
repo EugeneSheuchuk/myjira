6f63fc0cf53fbb6d7064c8295ddcaec220529fa6
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-20 11:05:56 +0300

    feat: add rootReducer and store

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './containers/App/App';
+import rootReducer from './store/rootReducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
