d8a861a7f93159bb0ca210db5dafe862601c7ab7
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-21 16:49:42 +0300

    feat:  add 'cancel' logic to 'add board' component

--- src/components/AddBoard/AddBoard.tsx
+++ src/components/AddBoard/AddBoard.tsx
@@ -16,10 +16,35 @@ class AddBoard extends React.Component<PropsType, StateType> {
       boardName: ''
     };
   }
+
+  typeBoardName = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    const boardName = e.currentTarget.value;
+    this.setState({ boardName })
+  };
+  pressKey = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if(e.keyCode === 27) {
+      this.props.cancel();
+      return;
+    }
+  };
+
   render() {
+    console.log(this.state);
+    const { cancel } = this.props;
+    const { boardName } = this.state;
     return(
       <div className="AddBoard">
-
+        <textarea
+          value={boardName}
+          placeholder="Board name"
+          autoFocus={true}
+          onChange={this.typeBoardName}
+          onKeyDown={this.pressKey}/>
+        <div className="AddBoard-choose">
+          <span className="AddBoard-choose-add">Add</span>
+          <span className="AddBoard-choose-cancel"
+                onClick={cancel}>Cancel</span>
+        </div>
       </div>
     );
   }
