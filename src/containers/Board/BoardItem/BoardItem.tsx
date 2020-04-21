81d0b7cd7a9eec60bc34c59c84407a11dba01b46
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-21 11:18:45 +0300

    feat:  textarea value bind with component state

--- src/containers/Board/BoardItem/BoardItem.tsx
+++ src/containers/Board/BoardItem/BoardItem.tsx
@@ -6,13 +6,15 @@ import AddButton from '../../../components/AddButton/AddButton';
 
 type State = {
   isAddingTask: boolean
+  newTaskText: string
 }
 
 class BoardItem extends React.Component<BoardType, State> {
   constructor(props: BoardType) {
     super(props);
     this.state = {
-      isAddingTask: false
+      isAddingTask: false,
+      newTaskText: '',
     };
   }
 
@@ -22,18 +24,26 @@ class BoardItem extends React.Component<BoardType, State> {
     this.setState({ isAddingTask: true });
   };
 
+  typeText = (e:React.FormEvent<HTMLTextAreaElement>) => {
+    const text = e.currentTarget.value;
+    this.setState({ newTaskText: text });
+  };
+
   render() {
     const { boardName } = this.props;
-    const newTaskText = this.state.isAddingTask
-      ? <textarea className='BoardItem-newTaskText'
+    const { isAddingTask, newTaskText } = this.state;
+    const newTask = isAddingTask
+      ? <textarea className='BoardItem-newTask'
                   placeholder='What needs to be done?'
-                  autoFocus={true}/>
+                  autoFocus={true}
+                  value={newTaskText}
+                  onChange={(e) => this.typeText(e)}/>
       : null;
     return (
       <div className="BoardItem">
         <div className="BoardItem-name">{boardName}</div>
         <div className="BoardItem-tasks"/>
-        {newTaskText}
+        {newTask}
         <AddButton imgURL={Add}
                    width={16}
                    height={16}
