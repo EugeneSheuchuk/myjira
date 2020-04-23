4e3c257d5bc73e7f8410de5bb5afab5eef3c0920
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-23 11:32:13 +0300

    feat: add logic to show/hide menu button

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -8,6 +8,7 @@ type StateType = {
   width: number;
   isResizing: boolean;
   isOpenMenu: boolean;
+  memoryWidth: number | null;
 };
 
 class App extends React.Component<{}, StateType> {
@@ -16,18 +17,41 @@ class App extends React.Component<{}, StateType> {
     this.state = {
       width: 225,
       isResizing: false,
-      isOpenMenu: true
+      isOpenMenu: true,
+      memoryWidth: null,
     };
   }
 
   toggleResizeBorder = (action: boolean) => {
-    this.setState({isResizing: action});
+    this.setState({ isResizing: action });
   };
 
   resizeBorder = (e: React.MouseEvent) => {
     const { width, isResizing } = this.state;
     if (isResizing) {
-      this.setState( {width: width + e.movementX});
+      this.setState({ width: width + e.movementX });
+    }
+  };
+
+  clickShowHideMenuButton = (e: React.MouseEvent | React.KeyboardEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const { isOpenMenu } = this.state;
+    let { width, memoryWidth } = this.state;
+    if (isOpenMenu) {
+      memoryWidth = width;
+      width = 15;
+      this.setState({ width, memoryWidth, isOpenMenu: false });
+    } else {
+      memoryWidth = memoryWidth === null ? 225 : memoryWidth;
+      width = memoryWidth;
+      this.setState({ width, memoryWidth, isOpenMenu: true });
+    }
+  };
+
+  pressEnter = (e: React.KeyboardEvent) => {
+    if (e.keyCode === 13) {
+      this.clickShowHideMenuButton(e);
     }
   };
 
@@ -42,15 +66,26 @@ class App extends React.Component<{}, StateType> {
       <div className="App" onMouseMove={this.resizeBorder}>
         <Sidebar />
         <Menu width={width} />
-        <div className='App-menuBorder'
-             style={{left: `calc(64px + ${width}px)`}}
-             onMouseDown={() => this.toggleResizeBorder(true)}
-             onMouseUp={() => this.toggleResizeBorder(false)}>
-          <div className='App-menuBorder-button'
-               style={{left: `calc(64px + ${width}px - 11px)`}}>
+        <div
+          className="App-menuBorder"
+          style={{ left: `calc(64px + ${width}px)` }}
+          onMouseDown={() => this.toggleResizeBorder(true)}
+          onMouseUp={() => this.toggleResizeBorder(false)}
+          role='button'
+          tabIndex={-1}
+          onKeyPress={() => {}}
+        >
+          <div
+            className="App-menuBorder-button"
+            style={{ left: `calc(64px + ${width}px - 11px)` }}
+            onClick={this.clickShowHideMenuButton}
+            role='button'
+            tabIndex={0}
+            onKeyDown={this.pressEnter}
+          >
             {isOpenMenu ? <span>&#8249;</span> : <span>&#8250;</span>}
           </div>
-          <div/>
+          <div />
         </div>
         <div className="App-article" style={style}>
           <MainBoard />
