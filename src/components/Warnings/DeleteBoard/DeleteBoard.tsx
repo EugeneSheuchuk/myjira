05849c54d14cf1642630c5f62d129f0640c9e525
Yauheni Sheuchuk <shevchuked@tut.by>
2020-05-07 16:37:37 +0300

    feat: add action to esc button on deleteBoard popup

--- src/components/Warnings/DeleteBoard/DeleteBoard.tsx
+++ src/components/Warnings/DeleteBoard/DeleteBoard.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './DeleteBoard.scss';
 import { DeleteWarningdProps } from '../../../types/types';
 
-
-
 const DeleteBoard: React.FC<DeleteWarningdProps> = ({ confirmAction, cancelAction }) => {
+
+  useEffect(()=> {
+    function pressKey(e: KeyboardEvent) {
+      if (e.key === 'Escape') cancelAction(e);
+    }
+    document.addEventListener('keydown', pressKey);
+    return () => {
+      document.removeEventListener('keydown', pressKey);
+    };
+  });
+
   return (
     <div className="DeleteBoard">
       <h2>Delete board</h2>
       <p>Are you sure to delete this board with all data?</p>
       <div className="DeleteBoard-buttons">
-        <button onClick={confirmAction} type="button" tabIndex={0}>
+        {/* eslint-disable-next-line jsx-a11y/no-autofocus */}
+        <button onClick={confirmAction} type="button" tabIndex={0} autoFocus={true}>
           Delete
         </button>
         <button onClick={cancelAction} type="button" tabIndex={0}>
